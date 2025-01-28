1c16b9f11dcce89abae23c0b53746fa0896667a3
zcdk077 <zackynautika@gmail.com>
2025-01-28 22:59:23 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,13 @@
-ALGO="spectre", // algo = dero, spectre, xelis
-URL="spr.tw-pool.com",
-PORT="14001",
-WALLET="spectre:qp6an6pkpfjxrummlv68grc6yyl3pcg0t7dawmar3l9q7m28ggjczvvz6zp57",
-WORK_NAME="Donations",
-THREADS=8,
+#!/bin/bash
+
+set -e
+
+ALGO="spectre" // algo = dero, spectre, xelis
+URL="spr.tw-pool.com"
+PORT="14001"
+WALLET="spectre:qp6an6pkpfjxrummlv68grc6yyl3pcg0t7dawmar3l9q7m28ggjczvvz6zp57"
+WORK_NAME="Donations"
+THREADS=8
 DONATION="" // Default 2.5% - Min. 1%
 
 if ($DONATION == null) {

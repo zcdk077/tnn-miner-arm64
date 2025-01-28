7cbb1d5a826588d035d8cfd0e0cd030ce3d87553
zcdk077 <zackynautika@gmail.com>
2025-01-28 22:56:46 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+ALGO="spectre", // algo = dero, spectre, xelis
+URL="spr.tw-pool.com",
+PORT="14001",
+WALLET="spectre:qp6an6pkpfjxrummlv68grc6yyl3pcg0t7dawmar3l9q7m28ggjczvvz6zp57",
+WORK_NAME="Donations",
+THREADS=8,
+DONATION="" // Default 2.5% - Min. 1%
+
+if ($DONATION == null) {
+    ./tnn-miner --$ALGO --stratum --daemon-address $URL --port $PORT --wallet $WALLET --worker-name $WORK_NAME --threads $THREADS --no-lock
+} else {
+    ./tnn-miner --$ALGO --stratum --daemon-address $URL --port $PORT --wallet $WALLET --worker-name $WORK_NAME --threads $THREADS --dev-fee $DONATION --no-lock
+}

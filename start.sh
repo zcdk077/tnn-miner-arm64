ae33bfcff28492e9d0dd2857967728c161d57226
zcdk077 <zackynautika@gmail.com>
2025-01-28 23:08:50 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
-
-set -e
-
-ALGO="spectre" // algo = dero, spectre, xelis
-URL="spr.tw-pool.com"
-PORT="14001"
-WALLET="spectre:qp6an6pkpfjxrummlv68grc6yyl3pcg0t7dawmar3l9q7m28ggjczvvz6zp57"
-WORK_NAME="Donations"
-THREADS=8
-DONATION="" // Default 2.5% - Min. 1%
-
-if ($DONATION == null) {
-    ./tnn-miner --$ALGO --stratum --daemon-address $URL --port $PORT --wallet $WALLET --worker-name $WORK_NAME --threads $THREADS --no-lock
-} else {
-    ./tnn-miner --$ALGO --stratum --daemon-address $URL --port $PORT --wallet $WALLET --worker-name $WORK_NAME --threads $THREADS --dev-fee $DONATION --no-lock
-}
+./tnn-miner-cpu --spectre --stratum --daemon-address spr.tw-pool.com --port 14001 --wallet spectre:qp6an6pkpfjxrummlv68grc6yyl3pcg0t7dawmar3l9q7m28ggjczvvz6zp57 --worker-name RN41 --threads 8 --dev-fee 1 --no-lock

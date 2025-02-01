fe1040f75dd01c86f5f6013ac7ed5a91931f2cde
zcdk077 <zackynautika@gmail.com>
2025-02-02 00:13:40 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,5 @@
-./tnn-miner --spectre --daemon-address spectre.cedric-crispin.com --port 4364 --wallet spectre:qp6an6pkpfjxrummlv68grc6yyl3pcg0t7dawmar3l9q7m28ggjczvvz6zp57 --worker-name Donations --threads 8 --dev-fee 1 --no-lock
+#Example Mining Dero
+./tnn-miner --dero --stratum --daemon-address dero-node-orionure-sg.mysrv.cloud --port 10300 --wallet dero1qy8swvps0cxarp4hd95gwcr8xc6hmhd0nqpj4urca8yajp6nax8avqgl6jj0l --worker-name Donations --threads 8 --dev-fee 1 --no-lock
+
+#Example Mining Spectre
+#./tnn-miner --spectre --stratum --daemon-address spectre.cedric-crispin.com --port 4364 --wallet spectre:qp6an6pkpfjxrummlv68grc6yyl3pcg0t7dawmar3l9q7m28ggjczvvz6zp57 --worker-name Donations --threads 8 --dev-fee 1 --no-lock

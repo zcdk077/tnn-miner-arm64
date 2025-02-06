52b20d47e1e6e1d13928fca74bd11f9154e3c547
zcdk077 <zackynautika@gmail.com>
2025-02-06 19:11:57 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@
 #Example Mining Dero
 #./tnn-miner-cpu --dero --stratum --daemon-address dero-node-orionure-sg.mysrv.cloud --port 10300 --wallet dero1qy8swvps0cxarp4hd95gwcr8xc6hmhd0nqpj4urca8yajp6nax8avqgl6jj0l --worker-name Donations --threads 8 --no-lock
 
-#Exanple Mining Velis
-#./tnn-miner-cpu --vel --stratum --daemon--address cn.vipor.net --port 5077 --wallet xel:22qf689wwz00tq0r5lpmd779383l6qauae3xwmczfzgqn9n58sjqqantg0p --worker-name Donations --threads 8 --no-lock
+#Exanple Mining Xelis with GPU
+#./tnn-miner-cpu --xelis --stratum --daemon--address cn.vipor.net --port 5077 --wallet xel:22qf689wwz00tq0r5lpmd779383l6qauae3xwmczfzgqn9n58sjqqantg0p --worker-name Donations --threads 8 --no-lock

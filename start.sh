f5aa0f4cd7fc896b1203e16cb00e510ae69a59de
zcdk077 <zackynautika@gmail.com>
2025-02-06 20:52:48 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,20 @@
-#Example Mining Spectre
+#Example Mining Spectre || CPU Only
 ./tnn-miner-cpu --spectre --stratum --daemon-address spr.tw-pool.com --port 14001 --wallet spectre:qp6an6pkpfjxrummlv68grc6yyl3pcg0t7dawmar3l9q7m28ggjczvvz6zp57 --worker-name Donations --threads 8 --no-lock
 
-#Example Mining Dero
+#Example Mining Dero || CPU Only
 #./tnn-miner-cpu --dero --stratum --daemon-address dero-node-orionure-sg.mysrv.cloud --port 10300 --wallet dero1qy8swvps0cxarp4hd95gwcr8xc6hmhd0nqpj4urca8yajp6nax8avqgl6jj0l --worker-name Donations --threads 8 --no-lock
 
-#Example Mining RandomX
+#Example Mining RandomX || CPU/GPU
+#Port Moneroocean.stream
+#10032: Old CPU/GPU
+#10128: Modern CPU/GPU
+#18192: CPU/GPU farm
+#20128: SSL/TLS
+#10001: Very old CPU (1000 diff)
 #./tnn-miner-cpu --randomx --stratum --daemon-address gulf.moneroocean.stream --port 10001 --wallet 43FZfSSGfddbUsyEVbiTmbY4aohwUGuJojF3ipdCyugdadtsoUhe6npFsAWKqwbbHi6FsyeJG4DghPdwxjXLsQJC9jpBz9t --worker-name Donations --threads 8 --no-lock
 
-#Exanple Mining Xelis
-#./tnn-miner-cpu -xelis --stratum --daemon--address cn.vipor.net --port 5077 --wallet xel:22qf689wwz00tq0r5lpmd779383l6qauae3xwmczfzgqn9n58sjqqantg0p --worker-name Donations --threads 8 --no-lock
+#Exanple Mining Xelis || GPU Only
+#./tnn-miner-cpu --xelis --stratum --daemon--address cn.vipor.net --port 5077 --wallet xel:22qf689wwz00tq0r5lpmd779383l6qauae3xwmczfzgqn9n58sjqqantg0p --worker-name Donations --threads 8 --no-lock
+
+#Example Mining Astrix || GPU Only
+#./tnn-miner-cpu --astrix --stratum --daemon-address au.mining4people.com --port 3394 --wallet astrix:qr9gcukhqtqlzuxh6qsz89lh64wdhrlveq58sqd7v09s96qv8w0wwujjvwz7h --worker-name Donations --threads 8 --no-lock

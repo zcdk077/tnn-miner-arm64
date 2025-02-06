e812eeeaa5596fcbffd760787c7f5eafe0fe11e9
zcdk077 <zackynautika@gmail.com>
2025-02-06 19:11:01 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,11 +4,5 @@
 #Example Mining Dero
 #./tnn-miner-cpu --dero --stratum --daemon-address dero-node-orionure-sg.mysrv.cloud --port 10300 --wallet dero1qy8swvps0cxarp4hd95gwcr8xc6hmhd0nqpj4urca8yajp6nax8avqgl6jj0l --worker-name Donations --threads 8 --no-lock
 
-#Example Mining RandomX
-#./tnn-miner-cpu --rx0 --stratum --daemon-address gulf.moneroocean.com --port 10128 --wallet 43FZfSSGfddbUsyEVbiTmbY4aohwUGuJojF3ipdCyugdadtsoUhe6npFsAWKqwbbHi6FsyeJG4DghPdwxjXLsQJC9jpBz9t --worker-name Donations --threads 8 --no-lock
-
 #Exanple Mining Velis
 #./tnn-miner-cpu --vel --stratum --daemon--address cn.vipor.net --port 5077 --wallet xel:22qf689wwz00tq0r5lpmd779383l6qauae3xwmczfzgqn9n58sjqqantg0p --worker-name Donations --threads 8 --no-lock
-
-#Example Astrix
-#./tnn-miner-cpu --aix --stratum --daemon-address au.mining4people.com --port 3394 --wallet astrix:qr9gcukhqtqlzuxh6qsz89lh64wdhrlveq58sqd7v09s96qv8w0wwujjvwz7h --worker-name Donations --threads 8 --no-lock

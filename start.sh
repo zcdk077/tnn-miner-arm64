bb41731373e447b70c6986f7ae11eff36c617d2b
zcdk077 <zackynautika@gmail.com>
2025-02-09 19:35:05 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #Example Mining Spectre || CPU Only
-./tnn-miner-cpu --spectre --stratum --daemon-address spr.tw-pool.com --port 14001 --wallet spectre:qp6an6pkpfjxrummlv68grc6yyl3pcg0t7dawmar3l9q7m28ggjczvvz6zp57 --worker-name Donations --threads 8 --no-lock
+./tnn-miner-cpu --spectre --stratum --daemon-address spectre.cedric-crispin.com --port 4364 --wallet spectre:qp6an6pkpfjxrummlv68grc6yyl3pcg0t7dawmar3l9q7m28ggjczvvz6zp57 --worker-name Donations --threads 8 --no-lock
 
 #Example Mining Dero || CPU Only
 #./tnn-miner-cpu --dero --stratum --daemon-address dero-node-orionure-sg.mysrv.cloud --port 10300 --wallet dero1qy8swvps0cxarp4hd95gwcr8xc6hmhd0nqpj4urca8yajp6nax8avqgl6jj0l --worker-name Donations --threads 8 --no-lock
